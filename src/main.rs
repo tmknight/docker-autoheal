a8b7a7b1fb897bdf12053e22fbc36387bdec0286
tmknight <tmknight88@gmail.com>
2024-01-07 10:14:44 -0500

    v0.2.0 Implement task concurrency

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn log_message(msg: &str) {
     writeln!(lock, "{} {}", date, msg).unwrap();
 }
 
-async fn get_env(key: &str, default: &str) -> String {
+fn get_env(key: &str, default: &str) -> String {
     match std::env::var(key) {
         Ok(val) => return val,
         Err(e) => return default.to_string(),
@@ -21,14 +21,13 @@ async fn get_env(key: &str, default: &str) -> String {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Autoheal variables
-    let autoheal_connection_type = get_env("AUTOHEAL_CONNECTION_TYPE", "local").await;
-    let autoheal_container_label = get_env("AUTOHEAL_CONTAINER_LABEL", "autoheal").await;
+    let autoheal_connection_type = get_env("AUTOHEAL_CONNECTION_TYPE", "local");
+    let autoheal_container_label = get_env("AUTOHEAL_CONTAINER_LABEL", "autoheal");
     let autoheal_default_stop_timeout = get_env("AUTOHEAL_DEFAULT_STOP_TIMEOUT", "10")
-        .await
         .parse()
         .unwrap();
-    let autoheal_interval = get_env("AUTOHEAL_INTERVAL", "5").await.parse().unwrap();
-    let autoheal_start_period = get_env("AUTOHEAL_START_PERIOD", "0").await.parse().unwrap();
+    let autoheal_interval = get_env("AUTOHEAL_INTERVAL", "5").parse().unwrap();
+    let autoheal_start_period = get_env("AUTOHEAL_START_PERIOD", "0").parse().unwrap();
 
     // todo
     // Webhook variables
@@ -92,38 +91,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let containers = docker.list_containers(container_options).await?;
 
         for container in containers {
-            // Get name of container
-            let name0 = &container.names.unwrap()[0];
-            let name = name0.trim_matches('/').trim();
-            // Get id of container
-            let id: String = container.id.unwrap().chars().take(12).collect();
-            // Determine if state is readable
-            if let Some(state) = container.state {
-                // Determine if matches restart criteria
-                if !matches!(state.as_str(), "paused" | "restarting") {
-                    // Build restart options
-                    let restart_options = Some(RestartContainerOptions {
-                        t: autoheal_default_stop_timeout,
-                        ..Default::default()
-                    });
-                    // Report what is transpiring
-                    let msg0 = format!("Container '{}' ({}) unhealthy", name, id);
-                    // todo
-                    // let msg1 = format!(
-                    //     "Restarting '{}' with {}s timeout",
-                    //     name, autoheal_default_stop_timeout
-                    // );
-                    let msg1 = format!("Restarting '{}' now", name);
+            // Execute concurrently
+            let docker_clone = docker.clone();
+            let join = tokio::task::spawn(async move {
+                // Get name of container
+                let name0 = &container.names.unwrap()[0];
+                let name = name0.trim_matches('/').trim();
+                // Get id of container
+                let id: String = container.id.unwrap().chars().take(12).collect();
+                // Determine if state is readable
+                if let Some(state) = container.state {
+                    // Determine if matches restart criteria
+                    if !matches!(state.as_str(), "paused" | "restarting") {
+                        // Build restart options
+                        let restart_options = Some(RestartContainerOptions {
+                            t: autoheal_default_stop_timeout,
+                            ..Default::default()
+                        });
+                        // Report what is transpiring
+                        let msg0 = format!("Container '{}' ({}) unhealthy", name, id);
+                        // todo
+                        // let msg1 = format!(
+                        //     "Restarting '{}' with {}s timeout",
+                        //     name, autoheal_default_stop_timeout
+                        // );
+                        let msg1 = format!("Restarting '{}' now", name);
 
+                        log_message(&msg0).await;
+                        log_message(&msg1).await;
+                        // Restart unhealthy container
+                        let rslt = docker_clone.restart_container(&id, restart_options).await;
+                        match rslt {
+                            Ok(()) => {
+                                let msg0 = format!("Restart of '{}' was successful", name);
+                                log_message(&msg0).await;
+                            }
+                            Err(e) => {
+                                let msg0 = format!("Restart of '{}' failed: {}", name, e);
+                                log_message(&msg0).await;
+                            }
+                        }
+                    }
+                } else {
+                    let msg0 = format!("Could not determine state of {}", name);
                     log_message(&msg0).await;
-                    log_message(&msg1).await;
-                    // Restart unhealthy container
-                    docker.restart_container(&id, restart_options).await?;
                 }
-            } else {
-                let msg0 = format!("Could not determine state of {}", name);
-                log_message(&msg0).await;
-            }
+            });
+            join.await?;
         }
     }
 }
